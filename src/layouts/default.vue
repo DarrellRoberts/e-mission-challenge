835e9f0ab73655c8e6ce640138de73c345df55a9
DarrellRoberts <mrdarrellroberts@gmail.com>
2025-01-15 13:29:07 +0100

    0001: main layout added

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,9 +1,12 @@
 <template>
-  <Header />
-  <Main />
-  <Footer />
+  <div class="website-grid">
+    <Header />
+    <Main />
+    <Footer />
+  </div>
 </template>
 
 <style>
 @import "../assets/stylesheets/main.css";
+@import "../assets/stylesheets/layout/main-layout.css";
 </style>
