fc779822bd1de5dde6e67952f0cc39c4a5f4d4d3
DarrellRoberts <mrdarrellroberts@gmail.com>
2025-01-17 16:45:24 +0100

    0001: made home page responsive, more visually pleasing and added skeleton loaders

---
+++ src/components/blog/SkeletonPosts.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const skeletonArray = computed<number[]>(() => {
+  return new Array(10);
+});
+</script>
+
+<template>
+  <div class="paginated-posts-container">
+    <v-container v-for="(_, i) in skeletonArray">
+      <v-skeleton-loader
+        :key="i"
+        class="pa-2 ma-2"
+        type="article"
+      ></v-skeleton-loader>
+    </v-container>
+  </div>
+</template>
+
+<style scoped>
+@import "../../assets/stylesheets/components/posts.css";
+</style>
